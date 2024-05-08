5e3400ceab458cb7c32f71efecd649a91f3328ba
laurenrickard13 <153468324+laurenhrickard@users.noreply.github.com>
2024-05-08 14:09:21 -0700

    Added link to github repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Lauren Rickard and is{" "}
+        <a
+          href="https://github.com/laurenhrickard/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
