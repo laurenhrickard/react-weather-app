268fb44cf48b09cd461d9f1f74e8aab21cf39839
laurenrickard13 <153468324+laurenhrickard@users.noreply.github.com>
2024-05-13 14:58:38 -0700

    Added one forecast day to act as template

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">Thu img 19 10</div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <WeatherIcon
+            code="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+            size={36}
+          />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,9 +17,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex">
-            <div>
-              <WeatherIcon code={props.data.icon} />
-            </div>
+            <WeatherIcon code={props.data.icon} size={52} />
           </div>
           <div>
             <WeatherTemperature celsius={props.data.temperature} />
